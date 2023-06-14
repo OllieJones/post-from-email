83bf2f1d27570557e986e4b4447dac9e25bacca4
OllieJones <olliejones@gmail.com>
2023-06-14 07:41:52 -0400

    #3 Embed data directly rather than iframeing it.

--- core/classes/class-run.php
+++ core/classes/class-run.php
@@ -3,9 +3,12 @@
 namespace Post_From_Email;
 // Exit if accessed directly.
 use DOMDocument;
+use DOMException;
 use DOMXPath;
 use Exception;
 use WP_Query;
+use Less_Parser;
+use Less_Exception_Parser;
 
 if ( ! defined( 'ABSPATH' ) ) {
   exit;
@@ -81,7 +84,12 @@ class Run {
    *
    * @return string Shortcode output.
    */
-  public function embed( array $atts = [], $content = null, $tag = '' ) {
+  public function embed( array $atts = [], string $content = null, string $tag = '' ) {
+
+    $html = get_post_meta( get_the_ID(), $atts['meta_tag'], true );
+    $doc  = $this->get_document_from_html( $html );
+
+    return $this->extract_body( $doc );
 
     wp_enqueue_style( 'post-from-email',
       POST_FROM_EMAIL_PLUGIN_URL . 'core/assets/css/post-from-email.css',
@@ -137,41 +145,46 @@ class Run {
   }
 
   private function get_file_url( $atts ): string {
-    global $post;
-    $name = empty( $atts['src'] ) ? POST_FROM_EMAIL_SLUG . $post->ID . '.html' : $this->get_filename( $atts['src'] );
+    $name = empty( $atts['src'] ) ? POST_FROM_EMAIL_SLUG . get_the_ID() . '.html' : $this->get_filename( $atts['src'] );
     $path = get_transient( POST_FROM_EMAIL_SLUG . '-file-' . $name );
     if ( $path ) {
       return $path;
     }
     $path = empty( $atts['src'] )
-      ? $this->load_meta_to_file( $atts, $name )
-      : $this->load_url_to_file( $atts['src'], $name );
+      ? $this->load_meta_to_iframeable_file( $atts, $name )
+      : $this->load_url_to_iframeable_file( $atts['src'], $name );
     set_transient( POST_FROM_EMAIL_SLUG . '-file-' . $name, $path, $this->timeout );
 
     return $path;
   }
 
-  private function load_html_to_file( &$html, $name ) {
-    $internal_errors = libxml_use_internal_errors( true );
-
-    $doc                     = new DOMDocument ( 1.0, 'utf-8)' );
-    $doc->preserveWhiteSpace = false;
-
-    $doc->loadHTML( $html );
-    libxml_use_internal_errors( $internal_errors );
-
-    $this->clean_doc( $doc );
-    $this->ingest_assets( $doc );
-    $this->annotate_doc( $doc );
-    libxml_use_internal_errors( $internal_errors );
+  /**
+   * Creates an iframeable file from html.
+   *
+   * @param string $html HTML to process.
+   * @param string $name Filename to use.
+   *
+   * @return string URL of local cached file.
+   * @throws DOMException
+   */
+  private function load_html_to_iframeable_file( &$html, $name ): string {
+    $doc = $this->get_document_from_html( $html );
+    /* Put on the iframeResizer js resizing. */
+    $this->put_iframeresizer_into_doc( $doc );
 
     return $this->write_sanitized_doc( $doc, $name );
   }
 
   /**
-   * @throws \DOMException
+   * Load a page from a URL and put it into a local cached file.
+   *
+   * @param string $url URL to load.
+   * @param string $filename Filename to use in local file system.
+   *
+   * @return string URL of local cached file.
+   * @throws DOMException
    */
-  private function load_url_to_file( $url, $name ): string {
+  private function load_url_to_iframeable_file( $url, $filename ): string {
 
     $response = cached_safe_remote_get( $url );
     $code     = wp_remote_retrieve_response_code( $response );
@@ -187,16 +200,20 @@ class Run {
       throw new Exception( $mesg );
     }
 
-    return $this->load_html_to_file( $response['body'], $name );
+    return $this->load_html_to_iframeable_file( $response['body'], $filename );
   }
 
   /**
-   * @throws \DOMException
+   * @param array  $atts Attributes for a shortcode renderer.
+   * @param string $filename Filename to use in local file system.
+   *
+   * @return string URL of local cached file.
+   * @throws DOMException
    */
-  private function load_meta_to_file( $atts, $name ): string {
+  private function load_meta_to_iframeable_file( $atts, $filename ): string {
     $html = get_post_meta( get_the_ID(), $atts['meta_tag'], true );
 
-    return $this->load_html_to_file( $html, $name );
+    return $this->load_html_to_iframeable_file( $html, $filename );
   }
 
   /**
@@ -226,8 +243,18 @@ class Run {
         error_log( 'Error in xpath expression ' . $scrub );
       }
     }
+  }
 
-    $els = @$xpath->query( '/html/body//img' );
+  /**
+   * Remove unnecessary parts of the document.
+   *
+   * @param DOMDocument $doc
+   *
+   * @return void
+   */
+  private function remove_tiny_img_tags_from_doc( DOMDocument &$doc ) {
+    $xpath = new DOMXPath( $doc );
+    $els   = @$xpath->query( '/html/body//img' );
     foreach ( $els as $el ) {
       /* Delete tiny images */
       $w = $el->getAttribute( 'width' );
@@ -238,6 +265,13 @@ class Run {
     }
   }
 
+  /**
+   * Ingest images (and other assets) from an HTML document, and updated the document to use local, ingested, copies.
+   *
+   * @param DOMDocument $doc HTML document to ingest.
+   *
+   * @return void
+   */
   private function ingest_assets( DOMDocument &$doc ) {
     $srcs  = array();
     $xpath = new DOMXPath( $doc );
@@ -250,34 +284,34 @@ class Run {
         $src          = $el->getAttribute( 'src' );
         $srcs[ $src ] = true;
       }
-      /* Ingest and cache the images. */
+      /* Ingest and cache the assets. */
       foreach ( $srcs as $src => $_ ) {
         $local_url     = $this->ingest_attachment( $src );
         $srcs [ $src ] = $local_url;
       }
-      /* Update the html to refer to the local images. */
+      /* Update the html to refer to the local assets. */
       foreach ( $els as $el ) {
-        /* Delete tiny images */
-        $w = $el->getAttribute( 'width' );
-        $h = $el->getAttribute( 'height' );
-        if ( strlen( $w ) > 0 && strlen( $h ) > 0 && ( $w * $h ) <= 16 ) {
-          $el->parentNode->removeChild( $el );
-        } else {
-          $src = $el->getAttribute( 'src' );
-          if ( ! empty ( $srcs [ $src ] ) ) {
-            $el->setAttribute( 'src', $srcs[ $src ] );
-          }
+        $src = $el->getAttribute( 'src' );
+        if ( ! empty ( $srcs [ $src ] ) ) {
+          $el->setAttribute( 'src', $srcs[ $src ] );
         }
       }
     }
   }
 
-  private function ingest_attachment( $src ): string {
+  /**
+   * Fetch an object from a URL and create an attachement post from it.
+   *
+   * @param string $url_to_ingest URL of the object (image, usually) to ingest.
+   *
+   * @return string URL of ingested copy on the local server.
+   */
+  private function ingest_attachment( string $url_to_ingest ): string {
     $local_url   = null;
     $args        = array(
       'post_type'           => 'attachment',
       'meta_key'            => '_source_url',
-      'meta_value'          => $src,
+      'meta_value'          => $url_to_ingest,
       'orderby'             => 'none',
       'nopaging'            => true,
       'ignore_sticky_posts' => true,
@@ -297,7 +331,8 @@ class Run {
       require_once( ABSPATH . 'wp-admin/includes/file.php' );
       require_once( ABSPATH . 'wp-admin/includes/image.php' );
       global $post;
-      $id         = media_sideload_image( $src, $post->ID, POST_FROM_EMAIL_SLUG . '_origin:' . $src, 'id' );
+      $id         =
+        media_sideload_image( $url_to_ingest, $post->ID, POST_FROM_EMAIL_SLUG . '_origin:' . $url_to_ingest, 'id' );
       $attachment = get_post( $id );
       $local_url  = $attachment->guid;
     }
@@ -314,7 +349,7 @@ class Run {
    * @return void
    * @throws \DOMException
    */
-  private function annotate_doc( DOMDocument &$doc ) {
+  private function put_iframeresizer_into_doc( DOMDocument $doc ) {
 
     /* append the iframeResizer content window tag. */
     $src = POST_FROM_EMAIL_PLUGIN_URL . 'core/assets/js/iframeResizer.contentWindow.min.js';
@@ -332,20 +367,135 @@ class Run {
    * Sanitize the document and write it to a file for iframe retrieval, creating the directory as needed.
    *
    * @param DOMDocument $doc The HTML document.
-   * @param string      $name The filename.
+   * @param string      $filename The filename in the local file system.
    *
    * @return string The url of the file written.
-   * @throws \DOMException
    */
-  private function write_sanitized_doc( DOMDocument &$doc, $name ) {
+  private function write_sanitized_doc( DOMDocument &$doc, string $filename ): string {
     $dirs    = wp_upload_dir();
     $dirname = $dirs['basedir'] . DIRECTORY_SEPARATOR . POST_FROM_EMAIL_SLUG;
     if ( ! @file_exists( $dirname ) ) {
       @mkdir( $dirname );
     }
-    $pathname = $dirname . DIRECTORY_SEPARATOR . $name;
+    $pathname = $dirname . DIRECTORY_SEPARATOR . $filename;
     file_put_contents( $pathname, $doc->saveHTML(), LOCK_EX );
 
-    return $dirs['baseurl'] . DIRECTORY_SEPARATOR . POST_FROM_EMAIL_SLUG . DIRECTORY_SEPARATOR . $name;
+    return $dirs['baseurl'] . DIRECTORY_SEPARATOR . POST_FROM_EMAIL_SLUG . DIRECTORY_SEPARATOR . $filename;
+  }
+
+  /**
+   * Turn a text string into a DOMDocument and tidy it up.
+   *
+   * @param string $html HTML document in a text string.
+   *
+   * @return DOMDocument The created document.
+   */
+  private function get_document_from_html( string &$html ): DOMDocument {
+    $internal_errors = libxml_use_internal_errors( true );
+
+    $doc = new DOMDocument ( 1.0, 'utf-8' );
+
+    $doc->preserveWhiteSpace = false;
+    $doc->loadHTML( $html );
+    $this->clean_doc( $doc );
+    $this->remove_tiny_img_tags_from_doc( $doc );
+    $this->ingest_assets( $doc );
+    libxml_use_internal_errors( $internal_errors );
+
+    return $doc;
+  }
+
+  const less_wrapper = <<<'LESSISMORE'
+div#post-from-email {
+    all: initial;
+    table, td {
+	    border-collapse: revert;
+	    border-width: 0;
+	    margin-bottom: 0;
+	    border-spacing: 0;
+	    caption-side: revert;
+	    color: revert;
+	    cursor: revert;
+	    direction: revert;
+	    empty-cells: revert;
+	    padding: 0;
+	};
+	
+    tr, td {
+      text-align: initial;
+    }	  
+LESSISMORE;
+
+  /**
+   * Extract the <body> from a doc as a <div> with the embedded style sheets included.
+   *
+   * @param DOMDocument $doc The document. It is mutated.
+   *
+   * @return false|string The extracted HTML.
+   * @throws DOMException
+   * @throws Less_Exception_Parser
+   */
+  private function extract_body( DOMDocument $doc ) {
+    require_once POST_FROM_EMAIL_PLUGIN_DIR . '/vendor/autoload.php';
+    $less_parser = new Less_Parser();
+
+    /* Fetch any style sheets, remove them, then scope-limit them with less */
+    $search      = new DOMXPath( $doc );
+    $els         = $search->query( '//style' );
+    $css = '';
+    /* Notice that we can't mutate the document while a Generator is in use on it. */
+    $stylesheets = array();
+    foreach ( $els as $el ) {
+      $css .= $el->nodeValue;
+      /* Notice that we can't mutate the document while a Generator is in use on it.
+       * So we materialize the Generator's results, then mutate. */
+      $stylesheets [] = $el;
+    }
+    foreach ( $stylesheets as $stylesheet ) {
+      $stylesheet->parentNode->removeChild( $stylesheet );
+    }
+    unset ($stylesheets);
+
+    $less = self::less_wrapper . $css . '}';
+    $less_parser->parse( $less );
+    $css                = $less_parser->getCss();
+    $style              = $doc->createElement( 'style' );
+    $style->textContent = $css;
+
+    /* Find the <body>. */
+    $body = null;
+    $els  = $search->query( '/html/body' );
+    foreach ( $els as $el ) {
+      $body = $el;
+      break;
+    }
+    /* Put in the changed style object. */
+    $body->insertBefore( $style, $body->firstChild );
+
+    /* Create a <div> with the same attributes as the <body> */
+    $attribute_nodes = array();
+    foreach ( $body->attributes as $attribute ) {
+      $attribute_nodes [] = $body->getAttributeNode( $attribute->name );
+    }
+    $div = $doc->createElement( 'div' );
+    foreach ( $attribute_nodes as $attribute_node ) {
+      $div->setAttributeNode( $attribute_node );
+    }
+    unset ($attribute_nodes);
+
+    $div->setAttribute( 'id', 'post-from-email' );
+
+    /* Move the body elements to the new <div> */
+    $child_nodes = array();
+    foreach ( $body->childNodes as $child_node ) {
+      $child_nodes [] = $child_node;
+    }
+    foreach ( $child_nodes as $child_node ) {
+      $div->appendChild( $child_node );
+    }
+    unset ( $child_nodes );
+
+    /* Extract the html from the newly populated <div> */
+    return $doc->saveHTML( $div );
   }
 }
