46706a1284e0d990cd2f38244d998cba6dcdf62c
OllieJones <olliejones@gmail.com>
2023-07-07 09:27:30 -0400

    Avoid attempts to expand shortcodes on post creation.

--- core/classes/class-make-post.php
+++ core/classes/class-make-post.php
@@ -51,26 +51,28 @@ class Make_Post {
    * @var array
    */
   private $upload;
+  /** @var string */
+  private $excerpt;
 
   /**
-   * @param array|null   $upload The uploaded email message.
+   * @param array|null $upload The uploaded email message.
    * @param WP_POST|null $profile The profile custom post to use as a template.
-   * @param array|null   $credentials The mailbox-access credentials.
+   * @param array|null $credentials The mailbox-access credentials.
    */
   public function __construct( array $upload, WP_POST $profile = null, array $credentials = null ) {
     require_once POST_FROM_EMAIL_PLUGIN_DIR . '/core/classes/class-log-post.php';
     require_once POST_FROM_EMAIL_PLUGIN_DIR . '/core/classes/util.php';
     $this->init( $upload, $profile, $credentials );
-    $this->log_item         = new Log_Post( $profile->ID );
+    $this->log_item = new Log_Post( $profile->ID );
     $this->log_item->source = $credentials['host'];
   }
 
   public function init( $upload, $profile, $credentials ) {
-    $this->version     = '1';
-    $this->namespace   = POST_FROM_EMAIL_SLUG;
-    $this->base        = 'upload';
-    $this->upload      = $upload;
-    $this->profile     = $profile;
+    $this->version = '1';
+    $this->namespace = POST_FROM_EMAIL_SLUG;
+    $this->base = 'upload';
+    $this->upload = $upload;
+    $this->profile = $profile;
     $this->credentials = $credentials;
   }
 
@@ -79,59 +81,59 @@ class Make_Post {
    */
   public function check() {
     $result = array();
-    $valid  = is_array( $this->upload )
-              && array_key_exists( 'headers', $this->upload )
-              && array_key_exists( 'html', $this->upload );
-    if ( ! $valid ) {
+    $valid = is_array( $this->upload )
+      && array_key_exists( 'headers', $this->upload )
+      && array_key_exists( 'html', $this->upload );
+    if( ! $valid ) {
       $error = array(
-        'code'    => self::INVALID_MESSAGE,
+        'code' => self::INVALID_MESSAGE,
         'message' => __( 'Incomplete or invalid email message', 'post-from-email' ),
       );
 
       $this->log_item->errors [] = $error;
-      $this->log_item->valid     = 0;
+      $this->log_item->valid = 0;
 
       return array( $result );
     }
 
     /* check the allowlist */
-    if ( array_key_exists( 'from', $this->upload['headers'] ) ) {
-      $from                 = $this->upload['headers']['from'];
+    if( array_key_exists( 'from', $this->upload['headers'] ) ) {
+      $from = $this->upload['headers']['from'];
       $this->log_item->from = $from;
     } else {
       $from = null;
     }
     $allowed = $this->sender_in_allowlist( $from );
 
-    if ( ! $allowed ) {
+    if( ! $allowed ) {
       /* translators: 1: a sanitized email address */
-      $message   = __( 'Message ignored because %1$s is not in the list of allowed senders', 'post-from-email' );
-      $message   = sprintf( $message, esc_attr( $from ) );
+      $message = __( 'Message ignored because %1$s is not in the list of allowed senders', 'post-from-email' );
+      $message = sprintf( $message, esc_attr( $from ) );
       $result [] = array(
-        'code'    => self::FROM_NOT_IN_ALLOWLIST,
+        'code' => self::FROM_NOT_IN_ALLOWLIST,
         'message' => $message,
       );
     }
 
-    if ( $this->credentials['dkim'] ) {
+    if( $this->credentials['dkim'] ) {
       $dkim_checked = $this->check_dkim_signature_exists( $this->upload['headers'] );
-      if ( ! $dkim_checked ) {
-        $message                   = __( 'Message ignored because it was not signed. ', 'post-from-email' );
-        $error                     = array(
-          'code'    => self::MISSING_SIGNATURE,
+      if( ! $dkim_checked ) {
+        $message = __( 'Message ignored because it was not signed. ', 'post-from-email' );
+        $error = array(
+          'code' => self::MISSING_SIGNATURE,
           'message' => $message,
         );
-        $result []                 = $error;
+        $result [] = $error;
         $this->log_item->errors [] = $error;
       } else {
         $dkim_verified = $this->verify_dkim_signature( $this->upload['headers'] );
-        if ( ! $dkim_verified ) {
-          $message                   = __( 'Message ignored because its signature was invalid. ', 'post-from-email' );
-          $error                     = array(
-            'code'    => self::INVALID_SIGNATURE,
+        if( ! $dkim_verified ) {
+          $message = __( 'Message ignored because its signature was invalid. ', 'post-from-email' );
+          $error = array(
+            'code' => self::INVALID_SIGNATURE,
             'message' => $message,
           );
-          $result []                 = $error;
+          $result [] = $error;
           $this->log_item->errors [] = $error;
         }
       }
@@ -141,20 +143,41 @@ class Make_Post {
   }
 
   /**
+   * Filters whether to call a shortcode callback.
+   *
+   * Returning a non-false value from filter will short-circuit the
+   * shortcode generation process, returning that value instead.
+   *
+   * @param false|string $result Short-circuit return value. Either false or the value to replace the shortcode with.
+   * @param string $tag Shortcode name.
+   * @param array|string $attr Shortcode attributes array or empty string.
+   * @param array $m Regular expression match array.
+   * @since 4.7.0
+   *
+   */
+  public function pre_do_shortcode_tag( $result, $tag, $attr, $m ) {
+    if( POST_FROM_EMAIL_SLUG === $tag ) {
+      return $this->excerpt;
+    }
+    return $result;
+  }
+
+
+  /**
    * Turn an email object into a post.
    *
    * @return string|WP_Error
    */
   public function process() {
 
-    if ( false && array_key_exists( 'to', $this->upload['headers'] ) ) {  //TODO plus addressing?
-      foreach ( $this->get_properties_from_email( imap_utf8( $this->upload['headers']['to'] ) ) as $category ) {
+    if( false && array_key_exists( 'to', $this->upload['headers'] ) ) {  //TODO plus addressing?
+      foreach( $this->get_properties_from_email( imap_utf8( $this->upload['headers']['to'] ) ) as $category ) {
         $categories [] = $this->maybe_insert_category( $category, $category, $category );
       }
     }
 
     $categories = wp_list_pluck( get_the_terms( $this->profile, 'category' ), 'term_id' );
-    $tags       = wp_list_pluck( get_the_terms( $this->profile, 'post_tag' ), 'term_id' );
+    $tags = wp_list_pluck( get_the_terms( $this->profile, 'post_tag' ), 'term_id' );
 
     try {
       $html = mb_convert_encoding( $this->upload['html'], 'HTML-ENTITIES', "UTF-8" );
@@ -168,74 +191,78 @@ class Make_Post {
       libxml_use_internal_errors( $internal_errors );
 
       $title = $this->getElementContents( $doc, '/html/head/title', '' );
-      if ( 0 === strlen( $title ) ) {
-        if ( array_key_exists( 'headers', $this->upload ) && array_key_exists( 'subject', $this->upload['headers'] ) ) {
+      if( 0 === strlen( $title ) ) {
+        if( array_key_exists( 'headers', $this->upload ) && array_key_exists( 'subject', $this->upload['headers'] ) ) {
           $title = imap_utf8( $this->upload['headers']['subject'] );
         }
       }
       $this->log_item->subject = $title;
 
       /* Use the date from the email header if available.  */
-      $date                   =
+      $date =
         array_key_exists( 'headers', $this->upload ) && array_key_exists( 'date', $this->upload['headers'] )
           ? $this->upload['headers']['date']
           : 'now';
-      $post_date_local        = ( new DateTimeImmutable( $date ) )->setTimezone( wp_timezone() );
+      $post_date_local = ( new DateTimeImmutable( $date ) )->setTimezone( wp_timezone() );
       $post_date_local_string = $post_date_local->format( "Y-m-d\TH:i:s" );
-      $post_date_utc_string   = $post_date_local->setTimezone( new DateTimeZone( 'UTC' ) )->format( "Y-m-d\TH:i:s" );
+      $post_date_utc_string = $post_date_local->setTimezone( new DateTimeZone( 'UTC' ) )->format( "Y-m-d\TH:i:s" );
 
-      if ( array_key_exists( 'plain', $this->upload ) ) {
-        $excerpt = $this->upload['plain'];
+      if( array_key_exists( 'plain', $this->upload ) ) {
+        $this->excerpt = $this->upload['plain'];
       } else {
-        $excerpt = $this->getElementContents( $doc, '/html/body', '' );
-        $excerpt = preg_replace( '/\s+/mu', ' ', $excerpt );
+        $this->excerpt = $this->getElementContents( $doc, '/html/body', '' );
+        $this->excerpt = preg_replace( '/\s+/mu', ' ', $this->excerpt );
       }
 
       $source_meta = POST_FROM_EMAIL_SLUG . '-source';
-      $content     = array();
-      $content []  = '[';
-      $content []  = POST_FROM_EMAIL_SLUG;
-      $content []  = ' tag="';
-      $content []  = $html_object_tag;
-      $content []  = '" ';
-      $content []  = ' meta_tag="';
-      $content []  = $source_meta;
-      $content []  = '" ';
-      $content []  = ']';
-      $post        = array(
-        'post_author'    => $this->profile->post_author,
+      $content = array();
+      $content [] = '[';
+      $content [] = POST_FROM_EMAIL_SLUG;
+      $content [] = ' tag="';
+      $content [] = $html_object_tag;
+      $content [] = '" ';
+      $content [] = ' meta_tag="';
+      $content [] = $source_meta;
+      $content [] = '" ';
+      $content [] = ']';
+      $post = array(
+        'post_author' => $this->profile->post_author,
         /* TODO make this a number of whole words */
-        'post_excerpt'   => substr( $excerpt, 0, 160 ),
-        'post_date'      => $post_date_local_string,
-        'post_date_gmt'  => $post_date_utc_string,
-        'post_content'   => implode( '', $content ),
-        'post_title'     => $title,
-        'post_status'    => $this->profile->post_status,
-        'post_category'  => $categories,
+        'post_excerpt' => substr( $this->excerpt, 0, 160 ),
+        'post_date' => $post_date_local_string,
+        'post_date_gmt' => $post_date_utc_string,
+        'post_content' => implode( '', $content ),
+        'post_title' => $title,
+        'post_status' => $this->profile->post_status,
+        'post_category' => $categories,
         'comment_status' => $this->profile->ping_status,
-        'ping_status'    => $this->profile->comment_status,
-        'tags_input'     => $tags,
+        'ping_status' => $this->profile->comment_status,
+        'tags_input' => $tags,
       );
-      $id          = wp_insert_post( $post, true, true );
-      if ( is_wp_error( $id ) ) {
+      /* Suppress any attempt to expand the shortcode from the not-yet-stored metadata. Yoast tries this. */
+      add_filter( 'pre_do_shortcode_tag', array( $this, 'pre_do_shortcode_tag' ), 10, 4 );
+      $id = wp_insert_post( $post, true, true );
+      remove_filter( 'pre_do_shortcode_tag', array( $this, 'pre_do_shortcode_tag' ), 10 );
+
+      if( is_wp_error( $id ) ) {
         $this->log_item->errors [] = array(
-          'code'    => self::POST_CREATION_FAILURE,
+          'code' => self::POST_CREATION_FAILURE,
           'message' => $id->get_error_message(),
         );
-        $this->log_item->valid     = 0;
+        $this->log_item->valid = 0;
 
         return $id;
       }
       update_post_meta( $id, $source_meta, $html, 'post' );
       $this->update_links( $id, $this->profile->ID );
       $this->log_item->post_id = $id;
-      $this->log_item->valid   = 1;
-    } catch ( Exception $ex ) {
+      $this->log_item->valid = 1;
+    } catch( Exception $ex ) {
       $this->log_item->errors [] = array(
-        'code'    => self::POST_CREATION_EXCEPTION,
+        'code' => self::POST_CREATION_EXCEPTION,
         'message' => $ex->getMessage(),
       );
-      $this->log_item->valid     = 0;
+      $this->log_item->valid = 0;
 
       return new WP_Error( 'imap', $ex->getMessage() );
     } finally {
@@ -250,11 +277,11 @@ class Make_Post {
    */
   private function maybe_insert_category( $category, $description, $nicename ) {
     $uploadCategoryId = term_exists( $category );
-    if ( ! $uploadCategoryId ) {
+    if( ! $uploadCategoryId ) {
       $uploadCategoryId = wp_insert_category( [
-        'cat_name'             => $category,
+        'cat_name' => $category,
         'category_description' => $description,
-        'category_nicename'    => $nicename,
+        'category_nicename' => $nicename,
       ] );
     }
 
@@ -265,8 +292,8 @@ class Make_Post {
    * Retrieve an element's text contents from a DOMDocument.
    *
    * @param DOMDocument $doc The document.
-   * @param string      $doc_path The xpath of the desired element. We return the first element found.
-   * @param string      $default The default value if the element isn't found.
+   * @param string $doc_path The xpath of the desired element. We return the first element found.
+   * @param string $default The default value if the element isn't found.
    *
    * @return mixed
    */
@@ -274,13 +301,13 @@ class Make_Post {
     $result = $default;
     try {
       $xpath = new DOMXPath( $doc );
-      $els   = $xpath->query( $doc_path );
-      foreach ( $els as $el ) {
+      $els = $xpath->query( $doc_path );
+      foreach( $els as $el ) {
         $result = $el->textContent;
         /* Return the first element found. */
         break;
       }
-    } catch ( Exception $ex ) {
+    } catch( Exception $ex ) {
       /* empty, intentionally */
     }
 
@@ -297,13 +324,13 @@ class Make_Post {
   private function get_properties_from_email( $to ) {
 
     $splits = explode( '@', $to, 2 );
-    $to     = $splits[0];
+    $to = $splits[0];
     $splits = explode( '+', $to, 2 );
-    if ( 2 === count( $splits ) && strlen( $splits[1] ) > 0 ) {
+    if( 2 === count( $splits ) && strlen( $splits[1] ) > 0 ) {
       $categories = $splits[1];
       $categories = explode( '|', $categories );
-      foreach ( $categories as $category ) {
-        if ( strlen( $category ) > 0 ) {
+      foreach( $categories as $category ) {
+        if( strlen( $category ) > 0 ) {
           yield $category;
         }
       }
@@ -318,18 +345,18 @@ class Make_Post {
    * @return bool True if the message should be allowed
    */
   private function sender_in_allowlist( $sender ) {
-    $allowed                 = false;
+    $allowed = false;
     $this->log_item->allowed = 0;
-    if ( is_array( $this->credentials )
-         && array_key_exists( 'allowlist', $this->credentials )
-         && is_string( $this->credentials['allowlist'] )
-         && strlen( $this->credentials['allowlist'] ) > 0 ) {
+    if( is_array( $this->credentials )
+      && array_key_exists( 'allowlist', $this->credentials )
+      && is_string( $this->credentials['allowlist'] )
+      && strlen( $this->credentials['allowlist'] ) > 0 ) {
       $allows = explode( "\n", sanitize_email_list( $this->credentials['allowlist'] ) );
 
-      if ( $sender ) {
-        foreach ( $allows as $allow ) {
-          if ( strlen( $allow ) > 0 ) {
-            if ( str_contains( strtolower( $sender ), strtolower( $allow ) ) ) {
+      if( $sender ) {
+        foreach( $allows as $allow ) {
+          if( strlen( $allow ) > 0 ) {
+            if( str_contains( strtolower( $sender ), strtolower( $allow ) ) ) {
               $this->log_item->allowed = 1;
 
               return true;
@@ -339,7 +366,7 @@ class Make_Post {
       }
     } else {
       $this->log_item->allowed = - 1;
-      $allowed                 = true;
+      $allowed = true;
     }
 
     return $allowed;
@@ -353,8 +380,8 @@ class Make_Post {
    * @return bool False if we should ignore the message.
    */
   private function check_dkim_signature_exists( $headers ) {
-    if ( array_key_exists( 'dkim', $this->credentials ) && $this->credentials['dkim'] ) {
-      $result                 = Pop_Email::check_dkim_signature_exists( $headers );
+    if( array_key_exists( 'dkim', $this->credentials ) && $this->credentials['dkim'] ) {
+      $result = Pop_Email::check_dkim_signature_exists( $headers );
       $this->log_item->signed = $result ? 1 : 0;
 
       return $result;
@@ -373,8 +400,8 @@ class Make_Post {
    * @return bool Falso if we should ignore the message.
    */
   private function verify_dkim_signature( $headers ) {
-    if ( array_key_exists( 'dkim', $this->credentials ) && $this->credentials['dkim'] ) {
-      $result                 = Pop_Email::verify_dkim_signature( $headers );
+    if( array_key_exists( 'dkim', $this->credentials ) && $this->credentials['dkim'] ) {
+      $result = Pop_Email::verify_dkim_signature( $headers );
       $this->log_item->signed = $result ? 1 : 0;
 
       return $result;
@@ -399,9 +426,9 @@ class Make_Post {
     update_post_meta( $post_id, $my_profile_key, $profile_id );
 
     $my_posts_key = POST_FROM_EMAIL_SLUG . '-posts';
-    $myposts      = get_post_meta( $profile_id, $my_posts_key, true );
-    $myposts      = is_array( $myposts ) ? $myposts : array();
-    $myposts []   = $post_id;
+    $myposts = get_post_meta( $profile_id, $my_posts_key, true );
+    $myposts = is_array( $myposts ) ? $myposts : array();
+    $myposts [] = $post_id;
     update_post_meta( $profile_id, $my_posts_key, $myposts );
   }
 }
